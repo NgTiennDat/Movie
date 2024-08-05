1045afe48763d4c98b4f5afb4653d199caa8fba7
NgTiennDat <126660251+NgTiennDat@users.noreply.github.com>
2024-08-05 14:20:31 +0700

    hidden ticket

--- src/main/java/com/datien/movie/ticket/Ticket.java
+++ src/main/java/com/datien/movie/ticket/Ticket.java
@@ -1,14 +1,14 @@
-package com.datien.movie.ticket;
-
-import com.datien.movie.common.BaseEntity;
-import lombok.*;
-
-@Getter
-@Setter
-@Builder
-@AllArgsConstructor
-@NoArgsConstructor
-public class Ticket extends BaseEntity {
-    private String title;
-
-}
+//package com.datien.movie.ticket;
+//
+//import com.datien.movie.common.BaseEntity;
+//import lombok.*;
+//
+//@Getter
+//@Setter
+//@Builder
+//@AllArgsConstructor
+//@NoArgsConstructor
+//public class Ticket extends BaseEntity {
+//    private String title;
+//
+//}
