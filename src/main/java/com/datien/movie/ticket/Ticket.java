5f8f35d0a2a3e2944ce6352de5cc7dc2352798e2
NgTiennDat <126660251+NgTiennDat@users.noreply.github.com>
2024-08-05 14:19:55 +0700

    add ticket atrribute

--- src/main/java/com/datien/movie/ticket/Ticket.java
+++ src/main/java/com/datien/movie/ticket/Ticket.java
@@ -1,11 +1,14 @@
-//package com.datien.movie.ticket;
-//
-//import lombok.*;
-//
-//@Getter
-//@Setter
-//@Builder
-//@AllArgsConstructor
-//@NoArgsConstructor
-//public class Ticket {
-//}
+package com.datien.movie.ticket;
+
+import com.datien.movie.common.BaseEntity;
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Ticket extends BaseEntity {
+    private String title;
+
+}
